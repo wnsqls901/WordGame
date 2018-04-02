fe23e661b24ecb3c43168b7f3604ad6548d749bb
Junbin Kwon <jkwon2@my.westga.edu>
2018-04-02 15:45:24 -0400

    Created UI for word game

--- src/edu/westga/cs1302/wordGame/Main.java
+++ src/edu/westga/cs1302/wordGame/Main.java
@@ -1,18 +1,21 @@
 package edu.westga.cs1302.wordGame;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("view/wordGameGui.fxml"));
+			AnchorPane pane = loader.load();
+			Scene scene = new Scene(pane);
+			primaryStage.setTitle("A Word Game by Junbin Kwon");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
---
+++ src/edu/westga/cs1302/wordGame/view/WordGameCodeBehind.java
@@ -0,0 +1,38 @@
+package edu.westga.cs1302.wordGame.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class WordGameCodeBehind {
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private Label textScore;
+
+    @FXML
+    private Label textSixLetter;
+
+    @FXML
+    private TextField textEnterWord;
+
+    @FXML
+    void handleEnter(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleShuffle(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleStartANewGame(ActionEvent event) {
+
+    }
+
+}
