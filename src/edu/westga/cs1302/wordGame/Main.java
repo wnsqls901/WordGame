73b0b898fac239da7263edfe374e07a1edd13dc0
Junbin Kwon <jkwon2@my.westga.edu>
2018-04-05 23:34:01 -0400

    Modified Check Style

--- src/edu/westga/cs1302/wordGame/Main.java
+++ src/edu/westga/cs1302/wordGame/Main.java
@@ -7,7 +7,15 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 
 
+/**
+ * The Class Main to run the application
+ * 
+ * @author Junbin Kwon
+ * @version 04-04-2018
+ */
 public class Main extends Application {
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -18,11 +26,16 @@ public class Main extends Application {
 			primaryStage.setTitle("A Word Game by Junbin Kwon");
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
